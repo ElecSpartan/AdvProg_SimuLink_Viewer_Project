9be09e3c3ad595b8fd6794fb474e0d88eb9e70c0
AmgadShrief <100617535+AmgadShrief@users.noreply.github.com>
2023-05-23 22:33:30 +0300

    Arrow

--- Simulink-viewer/src/main/java/com/example/simulinkviewer/Main.java
+++ Simulink-viewer/src/main/java/com/example/simulinkviewer/Main.java
@@ -2,6 +2,7 @@ package com.example.simulinkviewer;
 
 public class Main extends Application {
     static private List<Block> blocks = new ArrayList<Block>(); // for the blocks
+    static private List<Arrow> connections = new ArrayList<Arrow>(); // for the connections
 
 
 public static void addBlocks(Element rootElement, Document doc) {
@@ -87,5 +88,152 @@ public static void addBlocks(Element rootElement, Document doc) {
                 }
             }
         }
+    }
+    public static void addArrows(Element rootElement, Document doc) {
+        if(rootElement.getTagName().equals("System")){
+            NodeList lineList = doc.getElementsByTagName("Line");
+            for (int i = 0; i < lineList.getLength(); i++) {
+                Node lineNode = lineList.item(i);
+                //NodeList childNodes = lineNode.getChildNodes();
+                if (lineNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element lineElement = (Element) lineNode;
+                    NodeList branchList = lineElement.getElementsByTagName("Branch");
+                    // looping on the tags to find srcID srcPlace
+                    int srcId=0,srcPlace=0;
+                    for (int j = 0; j < lineElement.getElementsByTagName("P").getLength(); j++) {
+                        if(lineElement.getElementsByTagName("P").item(j).getAttributes().item(0).getTextContent().equals("Src")){
+                            String src=(lineElement.getElementsByTagName("P").item(j).getTextContent());
+                            String srcIdInfo=""; String srcPlaceInfo="";
+                            for(int k=0;k<src.length();k++){
+                                if(src.charAt(k)=='#')
+                                    break;
+                                else
+                                    srcIdInfo+=src.charAt(k);
+                            }
+                            int idx=-1;
+                            for(int k=0;k<src.length();k++){
+                                if(src.charAt(k)==':') {
+                                    idx=k+1;
+                                    break;
+                                }
+                            }
+                            for(int k=idx;k<src.length();k++)
+                                srcPlaceInfo+=src.charAt(k);
+                            srcId = Integer.parseInt(srcIdInfo);
+                            srcPlace = Integer.parseInt(srcPlaceInfo);
+                            //System.out.println(srcId+" "+srcPlace);
+                        }
+                    }
+
+                    //getting x and y of the line if there is no branches
+                    double x=0,y=0;
+                    String points="";
+                    if(getPIndexByName(lineElement, "Points")!=-1)
+                        points = lineElement.getElementsByTagName("P").item(getPIndexByName(lineElement, "Points")).getTextContent();
+                    if(points.length()>0) {
+                        String pointInfo="";
+                        for(int k=1;k<points.length();k++){
+                            if(points.charAt(k)==',')
+                                break;
+                            else
+                                pointInfo+=points.charAt(k);
+                        }
+                        x=Double.parseDouble(pointInfo);
+                        //to check if the string has y coordinations
+                        boolean yFlag=false;
+                        for(int k=0;k<points.length();k++){
+                            if(points.charAt(k)==';') yFlag=true;
+                        }
+                        if(yFlag){
+                            int index=-1;
+                            String yString = "";
+                            for(int k=points.length()-2;k>=0;k--){
+                                if(points.charAt(k)==' ') {
+                                    index=k+1;
+                                    break;
+                                }
+                            }
+                            for(int k=index;k<points.length()-1;k++)  yString+=points.charAt(k);
+                            y=Double.parseDouble(yString);
+                        }
+                    }
+
+                    /////////////////the object//////////////////////////
+                    Arrow arrow = new Arrow(srcId,srcPlace,x);
+
+
+                    // dstID if there is no branches
+                    if(branchList.getLength()==0){
+                        int destId=0 ;
+                        for (int j = 0; j < lineElement.getElementsByTagName("P").getLength(); j++) {
+                            if(lineElement.getElementsByTagName("P").item(j).getAttributes().item(0).getTextContent().equals("Dst")){
+                                String dst=(lineElement.getElementsByTagName("P").item(j).getTextContent());
+                                String dstIdInfo="";
+                                for(int k=0;k<dst.length();k++){
+                                    if(dst.charAt(k)=='#')
+                                        break;
+                                    else
+                                        dstIdInfo+=dst.charAt(k);
+                                }
+                                destId = Integer.parseInt(dstIdInfo);
+                                //System.out.println(destId);
+                            }
+                        }
+                        arrow.addDest(destId,y);
+                        connections.add(arrow);
+                    }
+
+                    //in case of branches
+                    else{
+                        //System.out.println(branchList.getLength());
+                        for (int j = 0; j < branchList.getLength(); j++) {
+                            Node branchNode = branchList.item(j);
+                            if (branchNode.getNodeType() == Node.ELEMENT_NODE) {
+                                Element branchElement = (Element) branchNode;
+                                int destIdBranch=0;
+                                double yBranch=0;
+                                for (int k = 0; k < branchElement.getElementsByTagName("P").getLength(); k++) {
+                                    if (branchElement.getElementsByTagName("P").item(k).getAttributes().item(0).getTextContent().equals("Dst")) {
+                                        String dstBranch=(branchElement.getElementsByTagName("P").item(k).getTextContent());
+                                        String dstIdInfo="";
+                                        for(int f=0;f<dstBranch.length();f++){
+                                            if(dstBranch.charAt(f)=='#')
+                                                break;
+                                            else
+                                                dstIdInfo+=dstBranch.charAt(f);
+                                        }
+                                        destIdBranch = Integer.parseInt(dstIdInfo);
+                                        //System.out.println(destIdBranch+" "+yBranch);
+                                        arrow.addDest(destIdBranch,yBranch);
+                                        connections.add(arrow);
+                                    }
+                                    else if(branchElement.getElementsByTagName("P").item(k).getAttributes().item(0).getTextContent().equals("Points")){
+                                        String pointsBranch=(branchElement.getElementsByTagName("P").item(k).getTextContent());
+                                        String yBranchString="";
+                                        int idx2=-1;
+                                        for(int f=pointsBranch.length()-2;f>=0;f--){
+                                            if(pointsBranch.charAt(f)==' ') idx2=f+1;
+                                        }
+                                        for(int f=idx2;f<pointsBranch.length()-1;f++)
+                                            yBranchString+=pointsBranch.charAt(f);
+                                        yBranch=Double.parseDouble(yBranchString);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private static int getPIndexByName(Element element, String name) {
+        NodeList pList = element.getElementsByTagName("P");
+        for (int i = 0; i < pList.getLength(); i++) {
+            Element pElement = (Element) pList.item(i);
+            if (pElement.getAttribute("Name").equals(name)) {
+                return i;
+            }
+        }
+        return -1; // Not found
     }
 }
